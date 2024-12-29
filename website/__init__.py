a590ce34e71721a2a2b3f639fbe7fbbacf6ee236
rashmi11mokashi <rashmimokashi2099@gmail.com>
2024-12-29 23:58:14 +0900

    DB facing error

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path 
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+DB_Name = "database.db"
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asdfghjkl'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_Name}'
+    print(app.config['SQLALCHEMY_DATABASE_URI'])  # Check the database URI
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix = '/')
     app.register_blueprint(auth, url_prefix = '/')
-
+    from .models import User, Note
+    create_database(app)
     return app 
+
+def create_database(app):
+    if not path.exists('website/' + DB_Name):
+        db.create_all()
+        print('DB created!')
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,41 @@
-from flask import Blueprint, render_template
+from flask import flash, Blueprint, redirect, render_template, request, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    data=request.form
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return render_template("home.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method=='POST':
+        email=request.form.get('email', '')
+        first_name=request.form.get('firstName', '') # should match the name in html form of GET
+        pwd1=request.form.get('password1', '')
+        pwd2=request.form.get('password2', '')
+        print(f"Email: {email}")
+        print(f"First Name: {first_name}")
+        print(f"Password1: {pwd1}")
+        print(f"Password2: {pwd2}")
+        if len(email) < 4:
+            flash('Email id is too short, must be greater than 4 characters', category = 'error')
+        elif len(first_name) < 2: 
+            flash('First name should be greater than 2 characters', category = 'error')
+        elif pwd1 != pwd2:
+            flash('Passwords does not match!', category = 'error')
+        else:
+            new_user = User(email=email, password=generate_password_hash(pwd1, method='pbkdf2:sha256'), first_name=first_name)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("sign_up.html")
